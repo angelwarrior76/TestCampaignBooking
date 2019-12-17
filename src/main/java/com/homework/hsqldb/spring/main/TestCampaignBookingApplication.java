e573fa9bd695fcc22623908042f94d437c146f8a
@angelwarrior76 <aleksandr.jusupov@gmail.com>
2019-12-17 15:20:09 +0200

    добавил подключение репозиториев и поиск entity

--- src/main/java/com/homework/hsqldb/spring/main/TestCampaignBookingApplication.java
+++ src/main/java/com/homework/hsqldb/spring/main/TestCampaignBookingApplication.java
@@ -4,14 +4,19 @@ import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Created by Alex on 04.05.2018.
  */
 @Configuration
+@EnableJpaRepositories("com.homework.hsqldb.spring.repository")
+@EntityScan("com.homework.hsqldb.spring.model")
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TestCampaignBookingApplication {
 
