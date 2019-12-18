092f0f7136181d6e607c10e0943890bf57ac1c12
@angelwarrior76 <aleksandr.jusupov@gmail.com>
2019-12-18 18:59:41 +0200

    убрал создание таблиц (выполняется при старте тестового контекста)

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,16 +1,4 @@
 /*==============================================================*/
-/* Table: campaigns                                             */
-/*==============================================================*/
-create table campaigns (
-  campaign_id          INT                  not null,
-  name                 VARCHAR(60)              null,
-  start_date           TIMESTAMP            not null,
-  finish_date          TIMESTAMP            not null,
-  status               INT                  not null,
-  constraint PK_CAMPAIGNS primary key (campaign_id)
-);
-
-/*==============================================================*/
 /* Index: CAMPAIGN_NAME_IND                                     */
 /*==============================================================*/
 create  index CAMPAIGN_NAME_IND on campaigns (
@@ -22,19 +10,6 @@ create  index CAMPAIGN_NAME_IND on campaigns (
 /*==============================================================*/
 create  index CAMPAIGN_STATUS_IND on campaigns (
   status
-);
-
-/*==============================================================*/
-/* Table: ads                                                   */
-/*==============================================================*/
-create table ads (
-  ad_id                INT                 not null,
-  campaign_id          INT                 not null,
-  asset_url            VARCHAR(60)         not null,
-  platforms            VARCHAR(10)         not null,
-  name                 VARCHAR(60)             null,
-  status               INT                 not null,
-  constraint PK_ADS primary key (ad_id, campaign_id)
 );
 
 /*==============================================================*/
@@ -50,8 +25,3 @@ create  index AD_NAME_IND on ads (
 create  index AD_STATUS_IND on ads (
   status
 );
-
-alter table ads
-  add constraint FK_ADS_FK_ADS_RE_CAMPAIGN foreign key (campaign_id)
-references campaigns (campaign_id)
-  on delete restrict on update restrict;
