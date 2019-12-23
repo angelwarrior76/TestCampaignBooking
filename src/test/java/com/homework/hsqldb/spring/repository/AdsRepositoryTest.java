a301701bc3b785720218cba9ed1790ebb001c138
@angelwarrior76 <aleksandr.jusupov@gmail.com>
2019-12-23 12:08:50 +0200

    подправил тест

--- src/test/java/com/homework/hsqldb/spring/repository/AdsRepositoryTest.java
+++ src/test/java/com/homework/hsqldb/spring/repository/AdsRepositoryTest.java
@@ -29,13 +29,14 @@ public class AdsRepositoryTest {
 
     @Autowired
     private AdsRepository ads;
+    private Campaign campaign = new Campaign(1);
 
     @Test
     public void whenFindingCustomerById_thenCorrect() {
         List<Platform> platforms = new ArrayList<>(0);
         platforms.add(Platform.WEB);
         platforms.add(Platform.ANDROID);
-        ads.save(new Ad(1, new Campaign(), "Test advertising", "https://testurl1.com", platforms, Status.PLANNED));
+        ads.save(new Ad(campaign, "Test advertising", "https://testurl1.com", platforms, Status.PLANNED));
         assertSame(ads.findById(1), Optional.class);
     }
 
@@ -44,8 +45,8 @@ public class AdsRepositoryTest {
         List<Platform> platforms = new ArrayList<>(0);
         platforms.add(Platform.WEB);
         platforms.add(Platform.ANDROID);
-        ads.save(new Ad(2, new Campaign(), "Test advertising 2", "https://testurl2.com", platforms, Status.PLANNED));
-        ads.save(new Ad(3, new Campaign(), "Test advertising 3", "https://testurl3.com", platforms, Status.PLANNED));
+        ads.save(new Ad(campaign, "Test advertising 2", "https://testurl2.com", platforms, Status.PLANNED));
+        ads.save(new Ad(campaign, "Test advertising 3", "https://testurl3.com", platforms, Status.PLANNED));
         assertSame(ads.findAll(), List.class);
     }
 
@@ -54,9 +55,9 @@ public class AdsRepositoryTest {
         List<Platform> platforms = new ArrayList<>(0);
         platforms.add(Platform.WEB);
         platforms.add(Platform.ANDROID);
-        ads.save(new Ad(4, new Campaign(), "Test advertising 4", "https://testurl4.com", platforms, Status.PLANNED));
+        ads.save(new Ad(campaign, "Test advertising 4", "https://testurl4.com", platforms, Status.PLANNED));
         Ad ad = ads.findById(4).orElseGet(()
-                -> new Ad(4, new Campaign(), "Test advertising 4", "https://testurl4.com", platforms, Status.PLANNED));
+                -> new Ad(4, campaign, "Test advertising 4", "https://testurl4.com", platforms, Status.PLANNED));
         assertEquals(ad.getName(), "Test advertising 4");
     }
 
